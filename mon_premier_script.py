831dac4fc2f1c27f3f5edfae86ef51256f0e5e26
KENFACK TEMGOUA Vanessa <99547234+vanessatemgoua@users.noreply.github.com>
2024-02-05 13:54:17 +0000

    feat: mon premier script python

--- mon_premier_script.py
+++ mon_premier_script.py
@@ -1,3 +1,6 @@
+import unittest
+
+
 def names(prenoms):
     more_than_seven = 0
     for prenom in prenoms:
@@ -8,5 +11,11 @@ def names(prenoms):
             print("Prenom inférieur ou égal à 7 : " + prenom)
     return more_than_seven
 
-prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
-print("Nombre de prénoms supérieurs à 7 : " + str(names(prenoms=prenoms)))
+class TestNamesMethod(unittest.TestCase):
+     def test_names(self):
+        prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
+        more_than_seven = names(prenoms=prenoms)
+        self.assertEqual(more_than_seven, 4)
+
+if __name__ == '__main__':
+    unittest.main()
